1ee04572f2490810cecda5a25ae830e67e099810
chenweichao <chenweichao@news.cn>
2014-07-08 14:34:50 +0800

    test junit

--- src/test/java/com/demo/TcmTest.java
+++ src/test/java/com/demo/TcmTest.java
@@ -1,17 +1,31 @@
 package com.demo;
 
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TcmTest {
-	// private static Logger logger = LoggerFactory.getLogger(TcmTest.class);
+	private static Logger logger = LoggerFactory.getLogger(TcmTest.class);
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		System.out.println("before test");
 	}
 
+	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 		System.out.println("AfterClass test");
 	}
 
+	@Test
+	public void testit() {
+		System.out.println("testit");
+	}
+
+	@Test
+	public void testit2() {
+		System.out.println("testit");
+	}
 }
