d618912557044a359b4843567591c50e5ffc160f
chenweichao <chenweichao@news.cn>
2014-09-29 16:32:34 +0800

    添加 字符串测试

--- src/test/java/com/demo/MainTest.java
+++ src/test/java/com/demo/MainTest.java
@@ -2,9 +2,18 @@ package com.demo;
 
 public class MainTest {
 	public static void main(String[] args) {
-		for (int i = 0; i < 50; i++) {
-			System.out.println((int) (Math.random() * 5));
-		}
+		// for (int i = 0; i < 50; i++) {
+		// System.out.println((int) (Math.random() * 5));
+		// }
+		String a = "foo";
+		String b = "foo";
 
+		System.out.println(a == b);
+		System.out.println(a.equals(b));
+		String c = new String("bar");
+		String d = new String("bar");
+
+		System.out.println(c == d);
+		System.out.println(c.equals(d));
 	}
 }
