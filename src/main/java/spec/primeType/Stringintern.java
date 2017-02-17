72d69c8901d8579ac8cb17156d6c16b531c9e302
KunkkaCoco <maximus.cwc@gmail.com>
2017-02-17 10:05:21 +0800

    commit

--- src/main/java/spec/primeType/Stringintern.java
+++ src/main/java/spec/primeType/Stringintern.java
@@ -7,13 +7,12 @@ public class Stringintern {
     public static void main(String[] args) {
         String hello = "Hello", lo = "lo";
 
+        System.out.println(hello.equals("Hel" + lo));
         System.out.println(hello == "Hello");
         System.out.println(Other.hello == hello);
-        System.out.println(spec.primeType.other.Other.hello == hello);
         System.out.println(hello == ("Hel" + "lo"));
         System.out.println(hello == ("Hel" + lo));
         System.out.println(hello == ("Hel"+lo).intern());
-
 
     }
 }
